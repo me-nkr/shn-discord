2e87a94aba08d31ed9f36b77e106871784aa23be
Naveen K R <thenaveenkr7@gmail.com>
2022-11-06 04:44:31 +0530

    update message handler to filter using filter words

--- events/message.js
+++ events/message.js
@@ -3,6 +3,9 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.MessageCreate,
     async execute (message) {
-        if ( /spam/.test(message.content) ) message.delete()
+        const { guildId } = message;
+
+        if (message.client.user.id !== message.author.id)
+        if ( message.client.dictionary[guildId] && message.client.dictionary[guildId].some(word => new RegExp(word, 'g').test(message.content) ) ) message.delete()
     }
 }
